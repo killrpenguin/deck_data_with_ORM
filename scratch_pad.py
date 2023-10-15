d40e3db0f4ce9a12b9f596a386e59b21d8d6a0c4
killrpenguin <dmcfarland8031@gmail.com>
2023-10-15 16:24:50 -0600

    Don't know what I should be putting in these.

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,49 +1,50 @@
-from contextlib import asynccontextmanager
-from concurrent.futures.thread import ThreadPoolExecutor
-import random
-import asyncio
 import seleniumwire
-from seleniumwire.request import *
+from seleniumwire import webdriver
 from seleniumwire.webdriver import EdgeOptions
+import asyncio
+import traceback
 
 
-class Adapter_Error(Exception):
-    pass
-
-
-@asynccontextmanager
-class Driver_Adapter(object):
+class Driver_Adapter:
     def __init__(self, link):
         self.link = link
         edge_options = EdgeOptions()
         edge_options.use_chromium = True
-        edge_options.add_argument("headless")
         edge_options.add_argument("start-maximized")
         edge_options.page_load_strategy = "eager"
         edge_options.add_argument("disable-gpu")
+        edge_options.add_argument("headless")
         self.driver = seleniumwire.webdriver.Edge(options=edge_options)
+        self.page = self.get_req()
 
-    def __aiter__(self):
-        return self
+    def __await__(self):
+        async def closure():
+            valid_resps = [200, 301, 302, 307, 404]
+            await asyncio.sleep(2)
+            for request in self.driver.requests:
+                if (request.url == self.link) and (request.response.status_code in valid_resps):
+                    print(f"Success! {request.response.status_code}")
+                    return self
+            return self
+        return closure().__await__()
 
-    def __anext__(self):
-        pass
-
-    async def get_page(self):
+    def get_req(self):
         try:
-            yield self.driver.get(self.link)
+            return self.driver.get(self.link)
         except Exception as e:
-            print(f"Error: {e}")
+            print(f'Exception: {e}\n{traceback.format_exc()}')
+async def task_coroutine(link):
+    driver = await Driver_Adapter(link=link)
+    return driver
 
-    @property
-    def page_status_code(self):
-        for request in self.driver.requests:
-            if request.url == self.link:
-                return request.response.status_code
 
-    @property
-    def get_random_agent(self):
-        with open("Master_User_Agents") as file:
-            user_agent_list = file.read().strip().split("\n")
-            self.user_agent = user_agent_list.pop(random.randint(0, len(user_agent_list)))
-            return self.user_agent
+async def main():
+    with open("mxlinks", "r") as link_list:
+        link_list = link_list.read().strip().split("\n")
+        tasks = [asyncio.create_task(task_coroutine(link)) for link in link_list]
+        for task in tasks:
+            await asyncio.gather(task)
+
+
+
+asyncio.run(main())
