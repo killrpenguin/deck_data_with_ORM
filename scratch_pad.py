207fa46efce1424661a6ca7e73a1d58cd9d398f0
killrpenguin <dmcfarland8031@gmail.com>
2023-10-15 19:19:31 -0600

    Driver Adapter Inherits from selenium.webdriver.Edge

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,39 +1,35 @@
 import seleniumwire
+import asyncio
 from seleniumwire import webdriver
 from seleniumwire.webdriver import EdgeOptions
-import asyncio
-import traceback
 
 
-class Driver_Adapter:
-    def __init__(self, link):
-        self.link = link
+class Driver_Adapter(webdriver.Edge):
+    def __init__(self, link, *args, **kwargs):
         edge_options = EdgeOptions()
         edge_options.use_chromium = True
         edge_options.add_argument("start-maximized")
         edge_options.page_load_strategy = "eager"
         edge_options.add_argument("disable-gpu")
         edge_options.add_argument("headless")
+        super().__init__(options=edge_options)
+        self.link = link
+        self.valid_resps = [200, 301, 302, 307, 404]
         self.driver = seleniumwire.webdriver.Edge(options=edge_options)
-        self.page = self.get_req()
+        self.page = self.driver.get(self.link)
 
     def __await__(self):
         async def closure():
-            valid_resps = [200, 301, 302, 307, 404]
-            await asyncio.sleep(2)
             for request in self.driver.requests:
-                if (request.url == self.link) and (request.response.status_code in valid_resps):
+                if (request.url == self.link) and (request.response.status_code in self.valid_resps):
                     print(f"Success! {request.response.status_code}")
+                    print(f"{self.driver.page_source}")
                     return self
             return self
         return closure().__await__()
 
-    def get_req(self):
-        try:
-            return self.driver.get(self.link)
-        except Exception as e:
-            print(f'Exception: {e}\n{traceback.format_exc()}')
-async def task_coroutine(link):
+
+async def task_coroutine(link: str):
     driver = await Driver_Adapter(link=link)
     return driver
 
@@ -44,7 +40,6 @@ async def main():
         tasks = [asyncio.create_task(task_coroutine(link)) for link in link_list]
         for task in tasks:
             await asyncio.gather(task)
-
 
 
 asyncio.run(main())
